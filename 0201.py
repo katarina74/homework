c72e5aa0a4007fbd5958dc0bca05df7549a1e17e
katarina74 <55535896+katarina74@users.noreply.github.com>
2019-09-28 21:56:51 +0300

    Delete 0201.py

--- 0201.py
+++
@@ -1,8 +0,0 @@
-def findErrors(str1):
-    numberOfErrors=0
-    for i in range(len(str1)-1):
-        if str1[i:i+2] in {"жы", "шы", "чя", "щя"}:
-            numberOfErrors=numberOfErrors+1
-    return numberOfErrors
-
-print("число ошибок: ", findErrors(input()))
